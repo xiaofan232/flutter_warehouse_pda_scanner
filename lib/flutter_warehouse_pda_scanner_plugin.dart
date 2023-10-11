c1990e5dd52e83522ff723c1f4ce3cc60c5db52d
wyb <wuyanbin@cdfsunrise.com>
2023-10-11 14:11:28 +0800

    [mod]fix warning

--- lib/flutter_warehouse_pda_scanner_plugin.dart
+++ lib/flutter_warehouse_pda_scanner_plugin.dart
@@ -6,7 +6,7 @@ class FlutterWarehousePdaScannerPlugin {
   static FlutterWarehousePdaScannerPlugin instance = FlutterWarehousePdaScannerPlugin._();
   // late final MethodChannel _channel;
   late final EventChannel _eventChannel;
-  late StreamSubscription _eventStreamSubscription;
+  // late StreamSubscription _eventStreamSubscription;
 
   final StreamController _getStringStreamController = StreamController.broadcast();
   Stream get getBarcodeResp =>_getStringStreamController.stream;
@@ -15,7 +15,7 @@ class FlutterWarehousePdaScannerPlugin {
   FlutterWarehousePdaScannerPlugin._() {
     _eventChannel =
         const EventChannel(eventChannelName); //定义接收底层操作系统主动发来的消息通道
-    _eventStreamSubscription = _eventChannel.receiveBroadcastStream().listen(
+    _eventChannel.receiveBroadcastStream().listen(
         _onEvent,
         onError: _onError); //注册消息回调函数;//广播流来处理EventChannel发来的消息
   }
